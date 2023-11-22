cdaa439d47a5ab122ffbf187642069619527d52d
Artur Sapek <me@artur.co>
2023-11-22 13:32:52 -0500

    add hash to filename for cache busting

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     entry: './index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
+        filename: 'index.[contenthash].js',
     },
     plugins: [
         new HtmlWebpackPlugin({
